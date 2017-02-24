1a33ee9b2084c34292a5223eb5b2d9c6af4804d1
BUNN1E5 <therealbunn1e5@gmail.com>
2017-02-23 23:31:06 -0800

    Moved function that displayed a box to the VisionCore class

--- CV/CV.cpp
+++ CV/CV.cpp
@@ -1,24 +1,24 @@
-// CV.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include <iostream>
 #include "opencv2\opencv.hpp"
+#include "VisionCore.h"
 
 using namespace std;
-
-int drawSquare(cv::Mat src, cv::Point2f points[], cv::Scalar color) {
-	cv::line(src, points[0], points[1], color);
-	cv::line(src, points[1], points[2], color);
-	cv::line(src, points[2], points[3], color);
-	cv::line(src, points[3], points[0], color);
-	return 0;
-}
 
 int main()
 {
 	cv::VideoCapture cap(0);
 	int lowerH = 17, lowerS = 157, lowerV = 132, upperH = 35, upperS = 255, upperV = 255;
-	float focalLength = 505, targetWidth = 3; 
+	float focalLength = 505, targetWidth = 3;
+
+	cv::Mat frame;
+	VisionCore vcore(focalLength);
+
+	while(true){
+		vcore.setBounds(cv::Scalar(upperH, upperS, upperV), cv::Scalar(lowerH, lowerS, lowerV));
+		cap >> frame;
+		VisionCore::VisionObject *obj = vcore.DetectObjects(frame);
+	}
+
     return 0;
 }
